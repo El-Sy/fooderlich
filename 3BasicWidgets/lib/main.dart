3840bc0a594a756df210352a71b70521051599c5
Lee Chuan <hyde.lee.chuan@gmail.com>
2022-01-03 21:21:11 +0800

    starting two tutorials

---
+++ 3BasicWidgets/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+import 'home.dart';
+
+void main() {
+  runApp(Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: const Home(),
+    );
+  }
+}
